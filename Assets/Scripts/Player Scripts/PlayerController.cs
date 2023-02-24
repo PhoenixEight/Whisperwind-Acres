3bcca85617584a620a6c2d62f42e4d5bdd2fe3d2
Phoenix-8 <PhoenixFireXYZ@gmail.com>
2023-02-24 10:23:55 -0600

    Fixed player movement

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -11,49 +11,88 @@ public class PlayerController : MonoBehaviour
     
     Vector2 movementInput;
     Rigidbody2D rb;
+    Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    bool canMove = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
-        //if movement is not 0, try to move
-        if(movementInput != Vector2.zero)
+        if(canMove)
         {
-            bool success = TryMove(movementInput);
-
-            if(!success)
+            //if movement is not 0, try to move
+            if(movementInput != Vector2.zero)
             {
-                success = TryMove(new Vector2(movementInput.x, 0));
+                Debug.Log("Trying to move");
+                bool success = TryMove(movementInput);
 
                 if(!success)
                 {
-                    success = TryMove(new Vector2(0, movementInput.y));
+                    success = TryMove(new Vector2(movementInput.x, 0));
+                    Debug.Log("Moving Horizontally");
+
+                    if(!success)
+                    {
+                        success = TryMove(new Vector2(0, movementInput.y));
+                        Debug.Log("Moving Vertically");
+                    }
                 }
+                animator.SetBool("isMoving", success);
+            } else
+            {
+                animator.SetBool("isMoving", false);
             }
         }
     }
 
     private bool TryMove(Vector2 direction)
     {
-        int count = rb.Cast(movementInput, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+        if(direction != Vector2.zero)
+        {
+            int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
 
             if(count==0)
             {
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                Debug.Log("Returned True");
                 return true;
             }
             else
             {
+                Debug.Log("Returned False");
                 return false;
             }
-
-        void OnMove(InputValue movementValue)
-        {
-            movementInput = movementValue.Get<Vector2>();
         }
+        else
+        {
+            Debug.Log("Returned False");
+            return false;
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+
+    void OnFire()
+    {
+        animator.SetTrigger("scytheAttack");
+    }
+
+    public void LockMovement()
+    {
+        canMove = false;
+    }
+
+    public void UnlockMovement()
+    {
+        canMove = true;
     }
 }
