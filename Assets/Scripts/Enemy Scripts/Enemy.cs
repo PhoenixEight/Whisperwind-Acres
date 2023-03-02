46f84f093af0e310a740085a771a6ace7bd2b97d
Phoenix-8 <PhoenixFireXYZ@gmail.com>
2023-03-02 11:01:03 -0600

    Fixed enemy health

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float health{
+    public float Health{
         set{
             health = value;
             if(health <= 0)
