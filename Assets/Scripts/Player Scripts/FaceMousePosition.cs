a1c3bc6949543a998f6bc67cb19462597307746b
Phoenix-8 <PhoenixFireXYZ@gmail.com>
2023-04-04 11:21:35 -0500

    Broke stuff ig idk

--- Assets/Scripts/Player Scripts/FaceMousePosition.cs
+++ Assets/Scripts/Player Scripts/FaceMousePosition.cs
@@ -5,58 +5,24 @@ using UnityEngine.InputSystem;
 
 public class FaceMousePosition : MonoBehaviour
 {
-    float raycast_depth = 100.0f;
-    LayerMask floor_layer_mask;
-    string floor_layer_name = "2D Floor";
-    
-    void Start()
-    {
-        floor_layer_mask = LayerMask.GetMask(floor_layer_name);
-    }
+    public Vector2 PointerPosition {get; set; }
+
+    private Vector2 pointerInput;
+
+    private InputActionReference pointerPosition;
 
     void Update()
     {
-    //     LookAtMouse();
-    // }
+        transform.right = (PointerPosition - (Vector2)transform.position).normalized;
 
-    // void LookAtMouse()
-    // {
-    	/** Face the mouse position while it's over the 2D game floor. **/
-
-    	RaycastHit[] mouse_hits = GetMouseFloorHits();
-
-        if(mouse_hits.Length == 1)
-        {
-            Vector3 hit_position = mouse_hits[0].point;
-            LookAtPosition(hit_position);
-        }
+        pointerInput = GetPointerInput();
+        PointerPosition = pointerInput;
     }
 
-    RaycastHit[] GetMouseFloorHits()
+    private Vector2 GetPointerInput()
     {
-    	/** Using the main camera and current mouse position, raycast through
-    		a specific layer looking for the '2D Floor' game object. **/
-
-    	Vector3 mouse_position = Mouse.current.position.ReadValue();
-    	
-        Ray mouse_ray = Camera.main.ScreenPointToRay(mouse_position);
-        return Physics.RaycastAll(mouse_ray, raycast_depth, floor_layer_mask);
-    }
-
-    void LookAtPosition(Vector3 hit_position)
-    {
-    	/** Turn this game object to face a specific position. Flatten the 3D position to
-    		local 2D space, to maintain parallel rotation with the game floor. **/
-
-        Vector3 local_hit_position = transform.InverseTransformPoint(hit_position);
-        local_hit_position.y = 0;
-        Vector3 look_position = transform.TransformPoint(local_hit_position);
-
-        // Using LookAt
-        transform.LookAt(look_position, transform.up);
-
-        // Using Quaternion
-        //Vector3 look_direction = look_position - transform.position;
-        //transform.rotation = Quaternion.LookRotation(look_direction, transform.up);
+        Vector3 mousePos = pointerPosition.action.ReadValue<Vector2>();
+        mousePos.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mousePos);
     }
 }
--- Assets/Scripts/Player Scripts/ScytheHitbox.cs
+++ Assets/Scripts/Player Scripts/ScytheHitbox.cs
@@ -6,8 +6,9 @@ public class ScytheHitbox : MonoBehaviour
 {
     public float scytheDamage = 1f;
     public Collider2D scytheCollider;
+    //public Vector2 attackOffset;
     public Vector3 faceRight = new Vector3(1, 0, 0);
-    public Vector3 faceLeft = new Vector3(-1, 0, 0);
+    public Vector3 faceLeft = new Vector3(/*attackOffset.x*/1 * -1, 0, 0);
     
     void Start()
     {
